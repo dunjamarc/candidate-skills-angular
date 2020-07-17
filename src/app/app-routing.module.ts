e3e97b9582a02cf9e2f5933a6986b5af4bb7cbac
Markovic <dmarkov@merann.ru>
2020-07-17 13:53:42 +0200

    Generate modules with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,11 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { Shell } from '@app/shell/shell.service';
 
 const routes: Routes = [
-  Shell.childRoutes([{ path: 'about', loadChildren: () => import('./about/about.module').then((m) => m.AboutModule) }]),
+  Shell.childRoutes([
+    { path: 'about', loadChildren: () => import('./about/about.module').then((m) => m.AboutModule) },
+    { path: 'skills', loadChildren: () => import('./skills/skills.module').then((m) => m.SkillsModule) },
+    { path: 'static', loadChildren: () => import('./static/static.module').then((m) => m.StaticModule) },
+  ]),
   // Fallback when no prior route is matched
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
