b7eca88d775d45fa6ea152b75e30282a1e89e6c5
Markovic <dmarkov@merann.ru>
2020-07-20 10:55:31 +0200

    Add gallery at static page

--- src/app/static/static.component.ts
+++ src/app/static/static.component.ts
@@ -9,4 +9,6 @@ export class StaticComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 }
--- src/app/static/static.module.ts
+++ src/app/static/static.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { StaticRoutingModule } from './static-routing.module';
 import { StaticComponent } from './static.component';
 
 @NgModule({
   declarations: [StaticComponent],
-  imports: [CommonModule, StaticRoutingModule],
+  imports: [CommonModule, StaticRoutingModule, NgbModule],
 })
 export class StaticModule {}
